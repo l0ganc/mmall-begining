6663acda609cdb6b3079c2db1785116cc20db1b1
l0ganc <jingyige@usc.edu>
2018-12-16 16:44:01 -0800

    user login, user register, user check, reset password, forget password

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,18 @@
+package com.mmall.common;
+
+/**
+ * @author logan
+ * @create 2018-12-16 3:12 PM
+ */
+public class Const {
+    public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role {
+        int ROLE_CUSTOMER = 0;   // 普通用户
+        int ROLE_ADMIN = 1;   // 管理员
+    }
+
+}
